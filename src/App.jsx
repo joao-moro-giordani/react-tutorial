1413eb4c06347789f1141b85941af58830feb372
JOAO GIORDANI <joao.040908@bento.local>
2026-08-26 11:06:45 -0300

    UseContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,15 @@
 import "./App.css";
-import heroImg from "./assets/hero.png";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import CaixaTexto from "./CaixaTexto";
-import Paragrafo from "./Paragrafo";
+import ThemeToggleButton from "./components/ThemeToggleButton";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Comece</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-
-        <CaixaTexto />
-        
-        <Paragrafo nome="Fulano" />
-        <Paragrafo nome="Beltrano" />
-
-      </section>
-
-      <div className="ticks"></div>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <ThemeProvider>
+      <div>
+        <nav>Menu de navegação</nav>
+        <ThemeToggleButton />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AppTarefas from './AppTarefas.jsx'
+import AppTarefas from './pages/tarefas/AppTarefas.jsx'
 import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppTarefas />
-    {/* <App /> */}
+    {/* <AppTarefas /> */}
+    <App />
   </StrictMode>,
 )
